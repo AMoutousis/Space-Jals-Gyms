49bd868f854e69f39e852f9f188ed8dd14fdb8ca
lauren <lauren.a.fryman@gmail.com>
2020-08-07 15:20:24 -0400

    First commit

---
+++ Space-JALS-Gyms/Club.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Space_JALS_Gyms
+{
+//    Void ClubInfo()
+//Print Club Info
+//Name
+//Address
+//ClubID
+
+    class Club
+    {
+    }
+}
---
+++ Space-JALS-Gyms/MultiMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Space_JALS_Gyms
+{
+//    Override Checkin(Club club)
+//Check if member is allowed to check in to specific club.Spoiler they will.
+//Void PointCheck()
+//Check how many points the member has.Add points on check in.
+//Override PrintInfo()
+//Print member info
+//fName
+//lName
+//Address
+
+    class MultiMember
+    {
+    }
+}
